78243c42686d20ac26d16dfd940eebb779ed1fab
joemurilloa <joemurillo95@gmail.com>
2023-10-18 14:31:06 -0600

    Actualiza la calculadora de préstamos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import './App.css';
-import LoanCalculator from './LoanCalculator'; // Importa el componente LoanCalculator
+  import logo from './logo.svg';
+  import './App.css';
+  import React from 'react';
+  import './App.css';
+  import LoanCalculator from './LoanCalculator'; // Importa el componente LoanCalculator
 
-function App() {
-  return (
-    <div className="App">
-      <LoanCalculator /> {/* Renderiza el componente LoanCalculator */}
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <LoanCalculator /> {/* Renderiza el componente LoanCalculator */}
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
